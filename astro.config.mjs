7b98675cffdaba381e11214fc29811866da130ff
matejikj <jakub.matejik@centrum.cz>
2026-02-23 17:47:13 +0100

    #5: Git pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+const repository = process.env.GITHUB_REPOSITORY?.split("/")[1];
+const isCi = process.env.GITHUB_ACTIONS === "true";
+const isUserOrOrgPages = repository?.toLowerCase().endsWith(".github.io");
+
 export default defineConfig({
   integrations: [tailwind()],
+  base: isCi && repository && !isUserOrOrgPages ? `/${repository}` : "/",
 });
-
